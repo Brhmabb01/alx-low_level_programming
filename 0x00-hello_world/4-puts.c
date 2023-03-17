7058a560bbd20587473ad6dd8b8e887b4538a883
Brhmabb01 <Brhmabb099@gmail.com>
2023-03-16 18:32:08 -0700

    helli puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #!/bin/bash
 #include <stdio.h>
-
+/**
+ * Decription: main - prints out a string to stout
+ * Return: 0 if success.
+ */
 int main (void)
 {
-puts ("/"programming is like building a multilingual puzzle")
-return (0)
+puts ("/"programming is like building a multilingual puzzle");
+return (0);
 }
