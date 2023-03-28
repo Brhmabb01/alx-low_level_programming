8270ce63487231e323eed9e9e74102a5e8078601
Brhmabb01 <Brhmabb099@gmail.com>
2023-03-27 21:07:26 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,21 +3,18 @@
 /**
  * puts2 - prints xter of a string
  * @str: used for string reference
- * Return: 0
+ * Return: void if iys correct
  */
 
 
 void puts2(char *str)
 {
-	int z = 0;
+int z;
 
-	while (str[z] != '\0')
-	{
-		if (z % 2 == 0)
-		{
-			_putchar(str[2]);
-		}
-		z++;
-	}
-	_putchar('\n');
+for  (z = 0; str[z]!= '\0'; z++)
+{
+if (z % 2 == 0)
+_putchar(str[z]);
+}
+_putchar('\n');
 }
